60f3a98776df71905c554d8bfc73c9dff2c0cdcd
Shahar Ben Tov <shahar.bentov@sightec.com>
2023-07-05 16:31:45 +0300

    dynamic menu all old-web

--- menu.php
+++ menu.php
@@ -1,6 +1,6 @@
 <div id="menu">
             <ul class="menu-list">
-                <a href="Main.html"><div class="image-container"><img src="css/logo.jpeg" alt="logo" id="logo"></div></a>
+                <a href="index.php"><div class="image-container"><img src="css/logo.jpeg" alt="logo" id="logo"></div></a>
                 <a href="Contact.html"><div class="image-container"><img src="css/contact.jpg" alt="contact" id="contact"><span class="image-text">Contact</span></div></a>
                 <a href="OrderTable.html"><div class="image-container"><img src="css/order_table.jpg" alt="order table" id="order_table"><span class="image-text">Order table</span></div></a>
                 <a href="Delivery.html"><div class="image-container"><img src="css/delivery.jpg" alt="delivery" id="delivery"><span class="image-text">Delivery</span></div></a>    
