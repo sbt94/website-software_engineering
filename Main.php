efbcc4c88c64a70e79ae9fb709a44b9bd79fcfa0
Shahar Ben Tov <shahar.bentov@sightec.com>
2023-07-05 16:26:10 +0300

    dynamic menu

--- Main.php
+++ Main.php
@@ -9,17 +9,7 @@
 </head>
 <body>
     <div id="container">
-        <div id="menu">
-            <ul class="menu-list">
-                <a href="Main.html"><div class="image-container"><img src="css/logo.jpeg" alt="logo" id="logo"></div></a>
-                <a href="Contact.html"><div class="image-container"><img src="css/contact.jpg" alt="contact" id="contact"><span class="image-text">Contact</span></div></a>
-                <a href="OrderTable.html"><div class="image-container"><img src="css/order_table.jpg" alt="order table" id="order_table"><span class="image-text">Order table</span></div></a>
-                <a href="Delivery.html"><div class="image-container"><img src="css/delivery.jpg" alt="delivery" id="delivery"><span class="image-text">Delivery</span></div></a>    
-                <a href="Cart.html"><div class="image-container"><img src="css/cart.jpg" alt="cart" id="cart"><span class="image-text">Cart</span></div></a>
-                <a href="Menu.html"><div class="image-container"><img src="css/menu.jpg" alt="menu_" id="menu_"><span class="image-text">Menu</span></div></a>
-                <a href="Recommendations.html"><div class="image-container"><img src="css/Recommendations.jpg" alt="Recommendations" id="Recommendations"><span class="image-text">Recommendations</span></div></a>
-            </ul>
-        </div>
+        <?php include 'menu.php'; ?>
             <div>
                 <header>
                     <h1>Welcome to Nadav's Diner</h1>
