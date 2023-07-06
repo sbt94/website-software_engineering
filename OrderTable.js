007e47a6f7bcd1c971bd2cdca11b0d706db61cfb
Shahar Ben Tov <shahar.bentov@sightec.com>
2023-07-06 16:51:50 +0300

    date and time .

--- OrderTable.js
+++ OrderTable.js
@@ -3,7 +3,7 @@
     var name = document.getElementById('name').value;
     var email = document.getElementById('email').value;
     var phone = document.getElementById('phone').value;
-    var date = document.getElementById('date').value;
+    var dateTime = document.getElementById('date').value;
     var time = document.getElementById('time').value;
     var partySize = document.getElementById('party-size').value;
 
