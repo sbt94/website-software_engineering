a0a33dcc66f0a8e89162463b75c34c6cb94d23af
Shahar Ben Tov <shahar.bentov@sightec.com>
2023-07-07 15:48:30 +0300

    contact

---
+++ Contact.php
@@ -0,0 +1,58 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Nadav's Diner - Contact Us</title>
+    <link rel="stylesheet" type="text/css" href="css/ContactUs.css">
+</head>
+<body>
+    <div id="container">
+        <?php include 'menu.php'; ?>
+    </div>
+    <div class="container">
+
+
+        <h1>Nadav's Diner</h1>
+        <h2>Contact Us</h2>
+
+        <div class="contact-info">
+            <h3>Owners:</h3>
+            <p>Ben-Tov Shahar: Phone number 0525442849, MailAddress - Shahar@gmail.com</p>
+            <p>Shneor Ran: Phone number 0526105567, MailAddress - Ran@gmail.com</p>
+        </div>
+
+        <div class="contact-info">
+            <h3>Restaurant Manager:</h3>
+            <p>Palas Michael: Phone number 0503973525, MailAddress - Mishca@gmail.com</p>
+        </div>
+
+        <div class="contact-info">
+            <h3>Chef:</h3>
+            <p>Alter Yotam: Phone number 0502545007, MailAddress - Yotam@gmail.com</p>
+        </div>
+
+        <form id="contact-form">
+            <div class="form-group">
+                <label for="name">Name:</label>
+                <input type="text" id="name" name="name" required="">
+            </div>
+
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" required="">
+            </div>
+
+            <div class="form-group">
+                <label for="message">Message:</label>
+                <textarea id="message" name="message" required=""></textarea>
+            </div>
+
+            <div class="form-group">
+                <button type="submit">Submit</button>
+            </div>
+        </form>
+    </div>
+    <script src="js/ContactUs.js"></script>
+
+</body>
+</html>
