00a0c6f39407fe9cd14fffa15def7210fb6f9582
Ubuntu <ubuntu@ip-172-31-95-196.ec2.internal>
2023-07-14 10:20:12 +0000

    reservation is working

--- reservation_page.php
+++ reservation_page.php
@@ -35,12 +35,13 @@
     <!-- Hidden fields for orderDetails and totalPrice -->
     <input type="hidden" id="orderDetails" name="orderDetails">
     <input type="hidden" id="totalPrice" name="totalPrice">
+    <input type="hidden" id="customerID" name="customerID">
 
     <input type="submit" value="Make a Reservation">
 </form>
         </div>
         <script>
-          document.addEventListener('DOMContentLoaded', (event) => {
+      document.addEventListener('DOMContentLoaded', (event) => {
     // Fetch order details and total price using customerID
     var customerID = localStorage.getItem('customerID');
     console.log('Starting fetch operation...');
@@ -56,13 +57,16 @@
         var totalPrice = data.reduce((total, item) => total + parseFloat(item.price), 0);
         document.getElementById('orderDetails').value = JSON.stringify(data);
         document.getElementById('totalPrice').value = totalPrice.toFixed(2);
+        document.getElementById('customerID').value = customerID;  // set customerID input value
         console.log('Order details:', document.getElementById('orderDetails').value);
         console.log('Total price:', document.getElementById('totalPrice').value);
+        console.log('CustomerID:', document.getElementById('customerID').value); // to verify customerID value
     })
     .catch((error) => {
         console.error('Error:', error);
     });
-  });
+});
+
     </script>  
     </body>
 </html>
